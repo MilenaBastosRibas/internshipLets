ac5825ac9c63af100fd2c1dab7af8d12860e6b88
Milena Bastos Ribas <milenabastosribas@hotmail.com>
2021-11-16 15:30:44 -0300

    modal, cep e mapa

---
+++ main.js
@@ -0,0 +1,73 @@
+// Modal
+var modal = document.getElementById("modal-subscribe");
+var btn = document.getElementById("btn-sub");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// buscar CEP
+function limpa_formulário_cep() {
+    document.getElementById('rua').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+    document.getElementById('ibge').value=("");
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    document.getElementById('rua').value=(conteudo.logradouro);
+    document.getElementById('bairro').value=(conteudo.bairro);
+    document.getElementById('cidade').value=(conteudo.localidade);
+    document.getElementById('uf').value=(conteudo.uf);
+    document.getElementById('ibge').value=(conteudo.ibge);
+}
+else {
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+}
+}
+
+function buscaCep(valor) {
+
+    var cep = valor.replace(/\D/g, '');
+
+    if (cep != "") {
+
+        var validacep = /^[0-9]{8}$/;
+
+        if(validacep.test(cep)) {
+            document.getElementById('rua').value="...";
+            document.getElementById('bairro').value="...";
+            document.getElementById('cidade').value="...";
+            document.getElementById('uf').value="...";
+            document.getElementById('ibge').value="...";
+
+            var script = document.createElement('script');
+
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+            document.body.appendChild(script);
+
+        }
+        else {
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    }
+    else {
+        limpa_formulário_cep();
+    }
+};
